a57ce2793f63fe7e7207d2e1058f8f4340d58de9
赵翔 <1084691005@qq.com>
2023-07-10 22:45:48 +0800

    初始化go项目，用户上线，Server给每个上线的服务发送消息

--- server.go
+++ server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -39,18 +41,58 @@ func (this *Server) Handler(conn net.Conn) {
 	// 当前链接的业务
 	// fmt.Println("链接建立成功")
 
-	user := NewUser(conn)
+	user := NewUser(conn, this)
 
 	// 用户上线，将用户加入到onlineMap中
-	this.mapLock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.mapLock.Unlock()
+	//this.mapLock.Lock()
+	//this.OnlineMap[user.Name] = user
+	//this.mapLock.Unlock()
+	user.OnLine()
 
 	// 广播当前用户上线消息
 	this.BroadCast(user, user.Name+"已上线")
 
+	isLive := make(chan bool)
+
+	go func() {
+		buf := make([]byte, 4096)
+		n, err := conn.Read(buf)
+		if n == 0 {
+			//this.BroadCast(user, user.Name+"下线")
+			user.OffLine()
+			return
+		}
+		if err != nil && err != io.EOF {
+			fmt.Println("Conn Read Error...", err)
+			return
+		}
+		//提取用户的输入(去除\n)
+		msg := string(buf[:n-1])
+		//消息广播
+		//this.BroadCast(user, msg)
+		user.DoMessage(msg)
+
+		// 用户任意消息代表处于活跃状态
+		isLive <- true
+	}()
+
 	// 当前handler阻塞
-	select {}
+	for {
+		//select - case穿透
+		select {
+		// 当前用户活跃，重置计时器，下面的也会执行
+		case <-isLive:
+		case <-time.After(time.Minute * 30):
+			//超时 将user关闭
+			user.SendMsgToCurClient("你被踢了")
+
+			//销毁
+			close(user.C)
+			conn.Close()
+			//退出当前handler
+			return //或者runtime.Goexit()
+		}
+	}
 }
 
 // 启动服务器的接口
@@ -89,6 +131,5 @@ func (this *Server) ListenMessager() {
 			client.C <- msg
 		}
 		this.mapLock.Unlock()
-
 	}
 }
--- user.go
+++ user.go
@@ -1,28 +1,112 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
-	Name string
-	Addr string
-	C    chan string
-	conn net.Conn
+	Name   string
+	Addr   string
+	C      chan string
+	conn   net.Conn
+	server *Server
 }
 
 // 创建一个用户
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 	user := &User{
-		Name: userAddr,
-		Addr: userAddr,
-		C:    make(chan string),
-		conn: conn,
+		Name:   userAddr,
+		Addr:   userAddr,
+		C:      make(chan string),
+		conn:   conn,
+		server: server,
 	}
 
 	// 启动监听当前user channel消息的goroutine
 	go user.ListenMessage()
 
 	return user
+}
+
+// 用户上线
+func (this *User) OnLine() {
+	// 用户上线，将用户加入到onlineMap中
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	this.server.BroadCast(this, this.Name+"已上线")
+}
+
+// 用户下线
+func (this *User) OffLine() {
+	// 用户下线，将用户从onlineMap移除
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	this.server.BroadCast(this, this.Name+"已下线")
+}
+
+// 给当前user对应的客户端发消息
+func (this *User) SendMsgToCurClient(msg string) {
+	this.conn.Write([]byte(msg))
+}
+
+// 用户处理消息
+func (this *User) DoMessage(msg string) {
+	if "who" == msg {
+		//查询当前有哪些用户在线
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onLineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线\n"
+			this.SendMsgToCurClient(onLineMsg)
+		}
+		this.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		//rename|张三
+		name := strings.Split(msg, "|")[1]
+		user := this.server.OnlineMap[name]
+		// 说明已经存在
+		if user != nil {
+			this.SendMsgToCurClient("当前用户已存在")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[name] = this
+			this.server.mapLock.Unlock()
+
+			this.Name = name
+			this.SendMsgToCurClient("更新成功：" + this.Name + "\n")
+		}
+	} else if len(msg) > 4 && msg[:3] == "to|" {
+		// to｜张三｜消息内容
+		// 获取对方的用户名
+		remoteName := strings.Split(msg, "|")[1]
+		if remoteName == "" {
+			this.SendMsgToCurClient("消息格式不正确,请使用 \"to｜张三｜消息内容\"格式.\n")
+			return
+		}
+		// 根据用户名获取user对象
+		remoteUser, ok := this.server.OnlineMap[remoteName]
+		if !ok {
+			this.SendMsgToCurClient("该用户不存在\n")
+			return
+		}
+		// 获取消息内容，通过user对象发送消息
+		content := strings.Split(msg, "|")[2]
+		if content == "" {
+			this.SendMsgToCurClient("请重发")
+			return
+		}
+		remoteUser.SendMsgToCurClient(this.Name + "对您说：" + content)
+	} else {
+		this.server.BroadCast(this, msg)
+	}
 }
 
 // 监听当前user channel，一旦有消息，发送给对应的客户端
